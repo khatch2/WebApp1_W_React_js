0e39324b126d31b234734330631151eeaf38b19c
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-08-13 18:09:26 +0200

    Again

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace WebApp1_W_React_js;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
